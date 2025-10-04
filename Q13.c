48d417ffa81539f85b5bd7919846c623e33958f3
ADITYAGAUTAM7009 <aditya.gautam7009@gmail.com>
2025-10-04 23:31:35 +0530

    Add files via upload

---
+++ Q13.c
@@ -0,0 +1,17 @@
+
+//Write a program to input a character and check whether it is a vowel or consonant using if–else.
+#include <stdio.h>
+
+int main() {
+	int year;
+	printf("Enter a year: ");
+	scanf("%d", &year);
+
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+		printf("%d is a leap year.\n", year);
+	} else {
+		printf("%d is not a leap year.\n", year);
+	}
+
+	return 0;
+}
