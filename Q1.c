0249b3837f0fbab8c588b58574ee8fe2bff257af
ADITYAGAUTAM7009 <aditya.gautam7009@gmail.com>
2025-10-04 20:11:57 +0530

    Q1.c

--- Q1.c
+++ Q1.c
@@ -1,9 +1,15 @@
-#include <stdio.h>
-int main()
-{
-    float x=5;
-    float y=2;
-    float z= 5/2;
-    printf ("%f" , z);
-    return 0;
+//Write a C program to add two numbers, take number from user
+#include<stdio.h>
+int main(){
+
+    float num1,num2,sum=0;
+
+    printf("Enter any two numbers: \n");
+    scanf("%f %f", &num1, &num2);
+
+    sum=num1 + num2;
+
+    printf("Sum of the two numbers is: %f", sum);
+
+        return 0;
 }
