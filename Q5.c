9924e7e2fa5df18d9e502cfbefada6b8922b884b
ADITYAGAUTAM7009 <aditya.gautam7009@gmail.com>
2025-10-13 10:11:07 +0530

    Add files via upload

---
+++ Q5.c
@@ -0,0 +1,11 @@
+ //Convert Celsius to Fahrenheit.
+ #include <stdio.h>
+ int main()
+ {
+  float celsius, farenheit;
+  printf("Enter temprature in celsius: ");
+  scanf("%f", &celsius);
+   farenheit =(9/5 * celsius ) +32;
+   printf("%.2f celsius is equal to %.2f farenheit\n ", celsius, farenheit);
+   return 0;
+ }
